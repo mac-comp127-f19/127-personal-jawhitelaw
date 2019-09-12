ca8b7f5a3614c213c335929989b3955eced3faaf
jawhitelaw <jwhitela@macalester.edu>
2019-09-12 10:31:42 -0500

    edited conway rule set and added new rule for 6 neighbors on a dead cell

---
+++ test/stringPractice/MakeEmphaticTest.java
@@ -0,0 +1,27 @@
+package stringPractice;
+
+import activityStarterCode.stringPractice.MakeEmphatic;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MakeEmphaticTest {
+
+    @Test
+    public void longStringTest() {
+        assertEquals("EMPHASIS!!!", stringPractice.MakeEmphatic.makeEmphatic("emphasis"));
+    }
+
+
+    @Test
+    public void shortStringTest() {
+        assertEquals("I!!!", stringPractice.MakeEmphatic.makeEmphatic("i"));
+    }
+
+    @Test
+    public void emptyStringTest() {
+        assertEquals("!!!", stringPractice.MakeEmphatic.makeEmphatic(""));
+    }
+
+
+}
---
+++ test/stringPractice/UppercaseFirstLetterTest.java
@@ -0,0 +1,35 @@
+package stringPractice;
+
+import activityStarterCode.stringPractice.UppercaseFirstLetter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UppercaseFirstLetterTest {
+
+    @Test
+    public void uppercaseStringTest() {
+        assertEquals("Uppercase", stringPractice.UppercaseFirstLetter.uppercaseFirstLetter("Uppercase"));
+    }
+
+    @Test
+    public void longStringTest() {
+        assertEquals("Uppercase", stringPractice.UppercaseFirstLetter.uppercaseFirstLetter("uppercase"));
+    }
+
+    @Test
+    public void shortStringTest() {
+        assertEquals("A", stringPractice.UppercaseFirstLetter.uppercaseFirstLetter("a"));
+    }
+
+    @Test
+    public void emptyStringTest() {
+        assertEquals("", stringPractice.UppercaseFirstLetter.uppercaseFirstLetter(""));
+    }
+
+    @Test
+    public void sameLetterTest() {
+        assertEquals("Eeee", stringPractice.UppercaseFirstLetter.uppercaseFirstLetter("eeee"));
+    }
+
+}
